Fixed applying shape to the signal, added signal scaling, redefined spectrogram function

letters/shape.py
@@ -1,7 +1,6 @@
+from typing import NoReturn
 import numpy as np
 from scipy.signal import chirp
-
-from utils.audioread import plot_sound
 
 
 class Shape:
@@ -13,17 +12,18 @@ class Shape:
         self.start_point = start  # In sec
         self.template = sound
 
-    def create_curve(self):
+    def create_curve(self) -> NoReturn:
         fs = self.template.sampling_rate
         t = np.arange(0, self.width * fs - 1, 1) / fs
 
         # For now in the middle of the range
-        f0 = 1
-        f1 = 15
+        f0 = fs/8
+        f1 = 3*fs/8
         t1 = self.width
 
+        # Creating chirp signal
         self.curve = chirp(t=t, f0=f0, f1=f1, t1=t1, method='linear')
 
-        plot_sound(self.curve, fs)
-
-        print(f'SHAPE --- Curve size: {len(self.curve)}')
+        # Scaling chirp signal
+        scale = np.mean(np.abs(self.template.data)) / np.mean(np.abs(self.curve))
+        self.curve = self.curve * scale

visualization/single_signal.py
@@ -33,10 +33,6 @@ class Signal:
             beg_idx = int(np.ceil(shape.start_point * self._sampling_rate))
             end_idx = int(np.ceil((shape.start_point + shape.width) * self._sampling_rate)) - 1
 
-            print(f'Slice size ({beg_idx}, {end_idx}): {end_idx-beg_idx+1}, Curve size: {len(shape.curve)}')
-
-            output1 = list(self.data[:beg_idx])
-            output2 = list(map(add, self.data[beg_idx:end_idx], shape.curve))
-            output3 = list(self.data[end_idx:])
-
-            self.data = np.array(output1 + output2 + output3)
+            self.data = np.array(list(self.data[:beg_idx])
+                                 + list(map(add, self.data[beg_idx:end_idx], shape.curve))
+                                 + list(self.data[end_idx:]))

visualization/spectrogram.py
@@ -19,8 +19,8 @@ class Spectrogram:
         self.signal = sound
 
     def calculate_spectrogram(self):
-        window_size = np.ceil(100*self.signal.sampling_rate/1000)  # 100 ms window
-        step_size = np.ceil(20*self.signal.sampling_rate/1000)  # 20 ms step
+        window_size = np.ceil(10*self.signal.sampling_rate/1000)  # 10 ms window
+        step_size = np.ceil(2*self.signal.sampling_rate/1000)  # 2 ms step
         n_per_segment = calculations.nextpow2(window_size)
 
         self.frqaxis, self.taxis, self.spectrogram = scipy.signal.spectrogram(x=self.signal.data,
@@ -28,16 +28,13 @@ class Spectrogram:
                                                                               nperseg=n_per_segment,
                                                                               noverlap=window_size-step_size,
                                                                               window='hamming')
-        print(f'Input data shape: {np.shape(self.signal.data)}')
-        print(f'Shape (number_of_rows, number_of_columns) of the outcome spectrogram: {np.shape(self.spectrogram)}')
 
     def normalize_spectrogram(self):
-        self.spectrogram = self.spectrogram / np.max(self.spectrogram)
+        self.spectrogram = self.spectrogram / np.min(self.spectrogram)
 
     def plot_spectrogram(self):
         # assert self.spectrogram is None, 'Spectrogram has not been calculated yet'
-        plt.imshow(self.spectrogram, aspect='auto', extent=[self.max_time//2, self.max_time,
-                                                            0, self.signal.sampling_rate])
+        plt.pcolormesh(self.taxis, self.frqaxis, self.spectrogram, shading='nearest')
         plt.title('Given audio signal in frequency and time domain')
         plt.xlabel('Time [s]')
         plt.ylabel('Frequencies [Hz]')
@@ -53,16 +50,10 @@ if __name__ == '__main__':
     read_samples, read_sr = audioread.read_file(path, max_t)
     sig = Signal(read_samples, read_sr)
 
-    # Creating and plotting spectrogram before
-    # spec = Spectrogram(sig, max_t)
-    # spec.calculate_spectrogram()
-    # spec.normalize_spectrogram()
-    # spec.plot_spectrogram()
-
     # Creating shape
-    letter_width = 1
-    letter_length = 5000
-    letter_start = 1
+    letter_width = 0.5
+    letter_length = 500
+    letter_start = 0
 
     shp = Shape(letter_width, letter_length, letter_start, sig)
     shp.create_curve()